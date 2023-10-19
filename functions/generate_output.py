a3f2c04d2861b67b9b28b83680fd385b717211d1
bujosa <davidbujosa@gmail.com>
2023-10-19 00:57:26 -0400

    feat: implement generate output

---
+++ functions/generate_output.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from functions.get_crypto_symbol import get_crypto_symbol
+
+def generate_output():
+    df = pd.read_csv('../data/dataset.csv')
+    unique_crypto_names = df['crypto_name'].unique()
+
+    df2 = pd.DataFrame(columns=['name', 'symbol'])
+
+    for crypto_name in unique_crypto_names:
+        symbol = get_crypto_symbol(crypto_name)
+        df2.loc[len(df2)] = [crypto_name, symbol]
+        
+    df2.to_csv('../data/output.csv', index=False)
+
