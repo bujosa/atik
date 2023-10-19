9f0080328e06a2d50d04087c601d66fb14f7588a
bujosa <davidbujosa@gmail.com>
2023-10-19 01:20:23 -0400

    feat: implement example.env

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 import pandas as pd
-from functions.get_crypto_symbol import get_crypto_symbol
 
-def generate_output():
-    df = pd.read_csv('data/dataset.csv')
-    unique_crypto_names = df['crypto_name'].unique()
+from core.types.asset import Asset
 
-    # Create new df for add new columns
-    df2 = pd.DataFrame(columns=['name', 'symbol'])
+# df = pd.read_csv('data/clean_data.csv')
 
-    for crypto_name in unique_crypto_names:
-        symbol = get_crypto_symbol(crypto_name)
-        df2.loc[len(df2)] = [crypto_name, symbol]
-        
-    # Export df2 to csv
-    df2.to_csv('data/output.csv', index=False)
+# for index, row in df.iterrows():
+#     print(row['symbol'])
 
-generate_output()
+# Use Asset class
+asset = Asset()
+asset.add()
